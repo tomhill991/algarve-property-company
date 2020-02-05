6d1f202736495851346a1c6221f3077459558d8b
Tom Hill <tom.hill991@gmail.com>
2020-02-05 23:43:12 +0900

    email edited

--- test/mailers/previews/capture_details_mailer_preview.rb
+++ test/mailers/previews/capture_details_mailer_preview.rb
@@ -3,7 +3,8 @@ class CaptureDetailsMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/capture_details_mailer/customer_details
   def customer_details
-    CaptureDetailsMailer.customer_details
+    customer = Customer.first
+    CaptureDetailsMailer.with(customer: customer).customer_details
   end
 
 end
